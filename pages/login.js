9465d1195ec5787f3a295f878a39fd2cc64c13d0
Matt Coffyn <matt.coffyn@gmail.com>
2022-02-01 10:08:30 +0000

    Fixed login imports

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,6 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import SignInPageComponent from '../components/SignInPageComponent';
 import { useUser } from '../context/user';
-import BaseLayout from '../components/layouts/BaseLayout';
 
 const Login = () => {
   const router = useRouter();
@@ -13,10 +11,6 @@ const Login = () => {
       <SignInPageComponent />
     </>
   );
-};
-
-Login.getLayout = function getLayout(page) {
-  return <BaseLayout>{page}</BaseLayout>;
 };
 
 export default Login;
