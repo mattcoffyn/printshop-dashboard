a1d21e77f2b58c560e196e2b2affed49ee3f753d
Matt Coffyn <matt.coffyn@gmail.com>
2022-02-02 09:23:31 +0000

    Add index page loading skeletons

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,8 +1,8 @@
 import {
   Box,
-  CircularProgress,
   Flex,
   GridItem,
+  Skeleton,
   Text,
   useColorModeValue,
 } from '@chakra-ui/react';
@@ -13,9 +13,6 @@ import { useUser } from '../../context/user';
 import { GET_ALL_PROCESS_PRODUCTS } from '../../components/queries/getAllProcessProducts';
 import DataTable from '../../components/Table';
 import { cleanAndCapitalise } from '../../lib/formatStrings';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import AppLayout from '../../components/layouts/AppLayout';
 
 const Dashboard = () => {
   const { user } = useUser();
@@ -24,62 +21,65 @@ const Dashboard = () => {
   const background = useColorModeValue('gray.100', 'gray.800');
   const { data, loading, error } = useQuery(GET_ALL_PROCESS_PRODUCTS);
 
-  if (loading) return <CircularProgress isIndeterminate color="green.300" />;
   if (error) return <p>{error.message}</p>;
 
   return (
     <>
       <GridItem gridColumn="1/17">
         <Flex align="center" mb="3.5rem" height="6rem">
-          <Text as="h1" fontSize="4xl" p={0} ml={8}>
-            {`Welcome ${user?.name}`}
-          </Text>
+          <Skeleton isLoaded={!loading}>
+            <Text as="h1" fontSize="4xl" p={0} ml={8}>
+              {`Welcome ${user?.name}`}
+            </Text>
+          </Skeleton>
         </Flex>
+        <Skeleton isLoaded={!loading}>
+          <Box
+            p={2}
+            ml={2}
+            borderWidth={1}
+            borderColor={cardBorder}
+            borderRadius="lg"
+            background={background}
+          >
+            {!loading && (
+              <DataTable
+                data={data.processProducts.map((order) => ({
+                  name: order.user.name,
+                  customerId: order.user.id,
+                  id: order.id,
+                  filmType: order.filmType.name,
+                  filmColour: order.filmColour.name,
+                  dev: order.noDevelop ? 'No' : 'Yes',
+                  scan: order.noScans ? 'No' : 'Yes',
+                  scanResolution: order.scanResolution?.name,
+                  status: cleanAndCapitalise(order.status),
+                  notes: order.notes,
+                }))}
+              />
+            )}
+          </Box>
+        </Skeleton>
+      </GridItem>
+      <Skeleton isLoaded={!loading}>
         <Box
-          p={2}
-          ml={2}
+          gridColumn="17/19"
+          p={3}
+          mt={2}
           borderWidth={1}
-          borderColor={cardBorder}
+          borderColor={border}
           borderRadius="lg"
-          background={background}
+          // bg={background}
         >
-          <DataTable
-            data={data.processProducts.map((order) => ({
-              name: order.user.name,
-              customerId: order.user.id,
-              id: order.id,
-              filmType: order.filmType.name,
-              filmColour: order.filmColour.name,
-              dev: order.noDevelop ? 'No' : 'Yes',
-              scan: order.noScans ? 'No' : 'Yes',
-              scanResolution: order.scanResolution?.name,
-              status: cleanAndCapitalise(order.status),
-              notes: order.notes,
-            }))}
+          <StatsCards
+            data={data}
+            background={background}
+            cardBorder={cardBorder}
           />
         </Box>
-      </GridItem>
-      <Box
-        gridColumn="17/19"
-        p={3}
-        mt={2}
-        borderWidth={1}
-        borderColor={border}
-        borderRadius="lg"
-        // bg={background}
-      >
-        <StatsCards
-          data={data}
-          background={background}
-          cardBorder={cardBorder}
-        />
-      </Box>
+      </Skeleton>
     </>
   );
-};
-
-Dashboard.getLayout = function getLayout(page) {
-  return <AppLayout>{page}</AppLayout>;
 };
 
 export default Dashboard;
